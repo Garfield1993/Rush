7cd72f8bef2af7bc8c4ced755fe163c6e48844b3
GarfieldZHU <Garfield.bupt@gmail.com>
2019-05-31 11:21:23 +0800

    update version

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
 fn main(){
+    // let test_b: bool = true;
+    // let test_c: char = 's';
+    
+    print!("{} ", test(10));
     println!("hello");
 }   
+
+fn test(i: usize) -> usize {
+    i*2
+}
