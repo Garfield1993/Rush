c9e8ad72e2f647af1213dee75c3694a5ec2865df
GarfieldZHU <Garfield.bupt@gmail.com>
2019-07-29 11:45:12 +0800

    Add DOM node

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,15 @@
 fn main(){
     // let test_b: bool = true;
     // let test_c: char = 's';
-    
+
+    let lambda = || 4;
+    println!("{}", lambda());
+
     print!("{} ", test(10));
     println!("hello");
-}   
+}
 
 fn test(i: usize) -> usize {
     i*2
 }
+
