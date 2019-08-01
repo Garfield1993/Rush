f2cd970fc4aa8cfc7c874a9e4dada4c849a2a28d
Garfield <garfield@bupt.edu.cn>
2019-08-01 23:18:45 +0800

    Use basic module

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,10 @@
+use dom::*;
+use css::*;
+
 fn main(){
     // let test_b: bool = true;
     // let test_c: char = 's';
 
-    let lambda = || 4;
-    println!("{}", lambda());
-
-    print!("{} ", test(10));
-    println!("hello");
-}
-
-fn test(i: usize) -> usize {
-    i*2
+    // TODO: start code
 }
 
